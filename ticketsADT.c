ad147f9ae610adbb63b365cfbc56ec4d559277c8
maguiGPorzio <mgonzalezporzio@itba.edu.ar>
2024-07-02 12:56:42 -0300

    feat: Agregue la funcion insertInfraction. Fix: Valide la asignacion de memoria en newTickets.

--- ticketsADT.c
+++ ticketsADT.c
@@ -1,4 +1,5 @@
 #include "ticketsADT.h"
+#include <errno.h>
 
 #define N_MONTH 12
 #define N_LETTER 'Z' - 'A' + 1
@@ -27,12 +28,11 @@ struct agencyNode {
   struct agencyNode *nextAgency;
 };
 
-typedef struct agencyNode *tAgencyList;
+typedef struct agencyNode *  tAgencyList;
 
 struct ticketsCDT {
   size_t beginYear;
   size_t endYear;
-  size_t descLength;
   size_t infractionsDim;
   tInfractionNode *infractions; // Vector de infracciones ordenados por id;
   tInfractionNode *firstByAmount;
@@ -40,16 +40,81 @@ struct ticketsCDT {
   tInfractionNode *firstByAlpha;
   tInfractionNode *currentByAlpha;
   size_t (*years)[N_MONTH]; 
-  size_t agenciesDim;
   tAgencyList agencies;
 };
 
 ticketsADT newTickets(size_t beginYear, size_t endYear) {
+  errno = 0;
   if (beginYear > endYear) {
     return NULL;
   }
+
   ticketsADT tickets = calloc(1, sizeof(*tickets));
+  if (tickets==NULL || errno == ENOMEM) {
+    return NULL;
+  }
+
   tickets->beginYear = beginYear;
   tickets->endYear = endYear;
   return tickets;
+}
+
+
+
+int insertInfraction(tInfraction infraction, ticketsADT tickets){
+
+  if (tickets->infractionsDim < infraction.id){
+
+    errno=0;
+
+    tickets->infractions=realloc(tickets->infractions, infraction.id * sizeof(tickets->infractions[0]));
+
+    if (tickets->infractions == NULL || errno == ENOMEM){
+      return 0;
+    }
+
+    int i=tickets->infractionsDim;
+    for (; i<infraction.id ; i++){
+      tickets->infractions[i].description=NULL;
+    }
+
+    tickets->infractionsDim=infraction.id;
+  }
+
+  if (tickets->infractions[infraction.id - 1].description != NULL){
+    return 0; 
+  }
+
+  tInfractionNode * ticket=&tickets->infractions[infraction.id - 1];
+  ticket->description=infraction.description;
+  ticket->infractionAmount=0;
+  ticket->nextByAlpha=NULL;
+  ticket->nextByAmount=NULL;
+  for (int i=0 ; i< N_LETTER + N_NUMS ; i++){
+    ticket->plateList[i]=NULL;
+  }
+
+  return 1; 
+}
+
+
+
+static tAgencyList addTicketToAgency(tAgencyList agencyNode, char * agency, size_t id);
+
+static void addTicketToYears(size_t (*years)[N_MONTH] , size_t year, size_t month);
+
+static void addTicketToInfraction(tInfractionNode * infractions, size_t id, char * plate);
+
+int insertTicket(tTicket ticket, ticketsADT tickets){
+
+  if (ticket.id > tickets->infractionsDim){ //esto si se hace en varias funciones conviene hacerlo en una macro 
+    return 0; 
+  }
+
+  tickets->agencies=addTicketToAgency(tickets->agencies, ticket.agency, ticket.id);
+  
+  addTicketToYears(tickets->years, ticket.year, ticket.month);
+
+  addTicketToInfraction(tickets->infractions, ticket.id, ticket.plate);
+
 }
--- ticketsADT.h
+++ ticketsADT.h
@@ -3,14 +3,14 @@
 
 #include <stdlib.h>
 
-typedef struct ticketsCDT *ticketsADT;
+typedef struct ticketsCDT * ticketsADT;
 
 typedef struct ticket {
   size_t id;
   char *agency;
   size_t year;
   char month;
-  char patente[10];
+  char plate[10];
 } tTicket;
 
 typedef struct infraction {
@@ -40,13 +40,25 @@ typedef struct year {
   size_t top[3];
 } tYear;
 
-
-
-
+/* Crea una nueva estructura donde se van a guardar los tickets.
+** Inicialmente esta vacia (no hay tickets).
+** Si no se pudo crear retorna NULL.
+** Almacena el rango de anios (beginYear y endYear inclusive) para los cuales se van a querer obtener los tres meses con mayores cantidades de multas (por anio).
+** Si beginYear es 0, significa que quiere las tres multas mas populares de cada anio registrado. 
+*/
 ticketsADT newTickets(size_t beginYear, size_t endYear);
 
+/* Vincula el id con la descripcion de la infraccion. 
+** Retorna 0 si el id ya estaba vinculado a una descripcion (no lo inserta).
+** Retorna 0 si falla la asignacion de memoria.
+** Retorna 1 si lo puedo vincular (lo inserta).
+*/
 int insertInfraction(tInfraction infraction, ticketsADT tickets);
 
+/* Inserta un ticket. 
+** Retorna 0 si el id de la infraccion no existe y no lo inserta.
+** Retorna 1 si lo pudo insertar.
+*/
 int insertTicket(tTicket ticket, ticketsADT tickets);
 
 void toBeginByAmount(ticketsADT tickets);
