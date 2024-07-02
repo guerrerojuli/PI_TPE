edf08c97a6b3ce1b04e40739dddd3d9af33c3a6f
maguiGPorzio <mgonzalezporzio@itba.edu.ar>
2024-07-02 15:24:06 -0300

    feat: Agregue la funcion auxiliar addTicketToAgency que se llama dentro de insertTicket.

--- ticketsADT.c
+++ ticketsADT.c
@@ -1,5 +1,6 @@
 #include "ticketsADT.h"
 #include <errno.h>
+#include <string.h>
 
 #define N_MONTH 12
 #define N_LETTER 'Z' - 'A' + 1
@@ -43,6 +44,8 @@ struct ticketsCDT {
   tAgencyList agencies;
 };
 
+
+
 ticketsADT newTickets(size_t beginYear, size_t endYear) {
   errno = 0;
   if (beginYear > endYear) {
@@ -63,29 +66,29 @@ ticketsADT newTickets(size_t beginYear, size_t endYear) {
 
 int insertInfraction(tInfraction infraction, ticketsADT tickets){
 
-  if (tickets->infractionsDim < infraction.id){
+  if (tickets->infractionsDim <= infraction.id){
 
     errno=0;
-
-    tickets->infractions=realloc(tickets->infractions, infraction.id * sizeof(tickets->infractions[0]));
-
+    tickets->infractions=realloc(tickets->infractions, (infraction.id + 1) * sizeof(tickets->infractions[0]));
     if (tickets->infractions == NULL || errno == ENOMEM){
       return 0;
     }
 
     int i=tickets->infractionsDim;
-    for (; i<infraction.id ; i++){
+    for (; i<=infraction.id ; i++){
       tickets->infractions[i].description=NULL;
     }
 
-    tickets->infractionsDim=infraction.id;
+    tickets->infractionsDim=infraction.id + 1;
   }
 
-  if (tickets->infractions[infraction.id - 1].description != NULL){
+  /*Si el id ya tiene vinculado una descripcion retorno 0*/
+  if (tickets->infractions[infraction.id].description != NULL){
     return 0; 
   }
 
-  tInfractionNode * ticket=&tickets->infractions[infraction.id - 1];
+  /*como ya se que el id no tiene vinculado una descripcion, agrego la descripcion y seteo todo en NUll o 0, retorno 1*/
+  tInfractionNode * ticket=&tickets->infractions[infraction.id];
   ticket->description=infraction.description;
   ticket->infractionAmount=0;
   ticket->nextByAlpha=NULL;
@@ -93,25 +96,78 @@ int insertInfraction(tInfraction infraction, ticketsADT tickets){
   for (int i=0 ; i< N_LETTER + N_NUMS ; i++){
     ticket->plateList[i]=NULL;
   }
+  return 1;
 
-  return 1; 
+}
+
+
+/*Funcion auxiliar para sumar una multa a la infraccion en el arreglo de infracciones de la agencia y actualizar, en caso de ser necesario, el maxId*/
+static tAgencyList
+addTicketToAgencyAux(tAgencyList agencyNode, size_t id){
+
+  agencyNode->inf[id]++;
+
+  /*Me fijo si ahora la infraccion que agregue es la que mayor multas tiene*/
+  if (agencyNode->inf[id] > agencyNode->inf[agencyNode->maxId]){
+    agencyNode->maxId=id;
+  }
+
+  return agencyNode;
+}
+
+/*Funcion auxiliar para crear un nodo de una agencia que es la primera vez que registra una infraccion*/
+static tAgencyList 
+newAgencyNode(tAgencyList agencyNode, char * agency, size_t id, size_t infractionsDim){
+
+  errno=0;
+  tAgencyList newAgency=malloc(sizeof(*newAgency));
+  if (newAgency == NULL || errno == ENOMEM){
+    return agencyNode; //Ante un error retorna la misma lista sin hacer cambios
+  }
+
+  newAgency->name=agency;
+  newAgency->nextAgency=agencyNode;
+  newAgency->maxId=id;
+  newAgency->inf=calloc(infractionsDim, sizeof(newAgency->inf[0]));
+  newAgency->inf[id]++;
+  
+  return newAgency;
+}
+
+/*Funcion auxiliar para a una agencia agregarle una infraccion, en caso de que la agencia todavia no tenga ninguna infraccion agrega la agencia a la lista*/
+static tAgencyList 
+addTicketToAgency(tAgencyList agencyNode, char * agency, size_t id, size_t infractionsDim){
+  int c;
+
+  if (agencyNode == NULL || (c=strcmp(agencyNode->name, agency) > 0)){
+    return newAgencyNode(agencyNode, agency, id, infractionsDim);
+  }
+
+  if (!c){
+    return addTicketToAgencyAux(agencyNode, id);
+  }
+
+  agencyNode->nextAgency=addTicketToAgency(agencyNode->nextAgency, agency, id, infractionsDim);
+  return agencyNode;
 }
 
 
 
-static tAgencyList addTicketToAgency(tAgencyList agencyNode, char * agency, size_t id);
 
 static void addTicketToYears(size_t (*years)[N_MONTH] , size_t year, size_t month);
 
 static void addTicketToInfraction(tInfractionNode * infractions, size_t id, char * plate);
 
+
+
+
 int insertTicket(tTicket ticket, ticketsADT tickets){
 
-  if (ticket.id > tickets->infractionsDim){ //esto si se hace en varias funciones conviene hacerlo en una macro 
+  if (ticket.id >= tickets->infractionsDim){ //esto si se hace en varias funciones conviene hacerlo en una macro 
     return 0; 
   }
 
-  tickets->agencies=addTicketToAgency(tickets->agencies, ticket.agency, ticket.id);
+  tickets->agencies=addTicketToAgency(tickets->agencies, ticket.agency, ticket.id, tickets->infractionsDim);
   
   addTicketToYears(tickets->years, ticket.year, ticket.month);
 
